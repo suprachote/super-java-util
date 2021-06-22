f4f8eca766725c96122c335a7eb76c5b86f36942
Suprachote Kongketyai <suprachote.kongketyai@ibm.com>
2021-06-22 17:07:46 +0700

    initial commit

---
+++ src/test/java/com/suprachote/superjavautil/SuperJavaUtilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suprachote.superjavautil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperJavaUtilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
